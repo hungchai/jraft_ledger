2e9d3a5dfc6b6fefa376d509726bb81919def35f
tomma <hungchai08@yahoo.com.hk>
2026-05-17 22:42:46 +0800

    init

--- src/main/java/com/example/ledger/mapper/TxnLogMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.ledger.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.example.ledger.model.TxnLogEntity;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface TxnLogMapper extends BaseMapper<TxnLogEntity> {
-}
